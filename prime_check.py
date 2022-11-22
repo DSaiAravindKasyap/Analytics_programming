237a345177c0f4f7da3973baabd9d75e36acba44
DSaiAravindKasyap <113101756+DSaiAravindKasyap@users.noreply.github.com>
2022-11-22 19:32:06 +0530

    Update prime_check.py

--- prime_check.py
+++ prime_check.py
@@ -3,12 +3,9 @@ This module implements the main functionality of Prime number checking.
 
 Author: D Sai Aravind Kasyap
 """
-
-
-class CheckPrime:
-    def prime_check(n):
-        for i in range(2, n):
-            if (n % i) == 0:
-                return False
+def prime_check(n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
         return True
 
